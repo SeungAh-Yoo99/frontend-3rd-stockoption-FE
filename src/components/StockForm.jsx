6dee1e7735adb12c00611fe8842ff9d40c99080f
min20ta <kimminjis2206@gmail.com>
2024-08-01 15:24:32 +0900

    feat : 주식창수정 주식api받아오기, 카테고리별 내용 불러오기

--- src/components/StockForm.jsx
+++ src/components/StockForm.jsx
@@ -1,35 +1,58 @@
 import React from 'react'
+import { useState,useEffect } from 'react';
 import { fetchStockRankingData } from "../api/api.js";
 import StockItemForm from './StockItemForm'
 import StockFilter from './StockFilter'
 
-function StockForm() {
-  
 
-  const fetchDataAsync = async () => {
+
+
+function StockForm() {
+
+  const [items, setItems] = useState([]);
+  const [selectedCategory, setSelected] = useState("high");
+
+  const handleSelect = (e) => {
+    let a= e.target.value;
+    setSelected(a);
+    console.log(a)
+  };
+
+
+  useEffect(() => {
+  
+    const fetchDataAsync = async () => {
     try {
-      const result = await fetchStockRankingData('0001', '0');
-      console.log(result[0]); // 결과를 콘솔에 출력
+      let result;
+      result = await fetchStockRankingData('0001', selectedCategory === 'high' ? "0" : "1");
+      setItems(result);
+      // console.log(result);
+  
     } catch (error) {
       console.error("Error fetching data:", error); // 오류를 콘솔에 출력
     }
-  };
+  }
+    
+    fetchDataAsync();
+},[selectedCategory]);
 
-  fetchDataAsync();
 
   return (
     <>
-    <StockFilter ></StockFilter>
+    <StockFilter handleFilter={handleSelect}></StockFilter>
     <div className= "grid grid-flow-col  row-span-${order.products.length} lg:grid-cols-5 grid-cols-5 gap-12 items-center ">
         <div>data순위</div>
         <div>종목명</div>
         <div>현재가</div>
-        <div>전일대비,전일대비부호</div>
+        <div>전일대비</div>
         <div>전일대비율</div>
     </div>
-    <div className="w-[100%] my-[1%] border-[1px] border-lightGray/30"></div>
+    <div className="w-[100%] my-[1%] border-[1px] border-black/70"></div>
     <div></div>
-    <StockItemForm></StockItemForm>
+   
+    {items.map(item => <StockItemForm item={item} key={item.data_rank}/>)}
+
+  
     
     </>
   )
